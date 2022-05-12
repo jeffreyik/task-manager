d08adae90130ac74b14280fab0c3d553b7a78a20
jeffreyik <ikechukwujeffrey19@gmail.com>
2022-05-12 15:06:31 +0100

    worked on the new dropdown menu

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
 const list = document.querySelector('.list');
-const nav = document.querySelector('#new-btn');
+const body = document.querySelector('body');
 const categoryDropdown = document.querySelector('.category-list');
-const addBtn = document.querySelector('#add-btn');
+const addBtn = document.querySelector('.add-btn');
+
+// Category buttons
+// const workBtn = document.querySelector('work-btn');
+// const personalBtn = document.querySelector('personal-btn');
+// const travelBtn = document.querySelector('travel-btn');
+
 
 addBtn.addEventListener('click', addTodoItem);
-newBtn.addEventListener('click', dropDown);
+body.addEventListener('click', dropDown);
+categoryDropdown.addEventListener('click', addNew);
+
+function addNew(e) {
+    let target = e.target;
+    if (target.classList.contains("work-btn")) {
+        createWork();
+    } else if (target.classList.contains("personal-btn")) {
+        createPersonal();
+    } else if (target.classList.contains("travel-btn")) {
+        createTravel();
+    }
+}
+
+
+// function createWork() {
+//     const workWrapper = document.createElement('div');
+//     workWrapper.classList.add('list');
+//     workWrapper.classList.add('work');
+
+//     const categoryLabel = document.createElement('div');
+//     categoryLabel.classList.add('category');
+//     categoryLabel.innerHTML = '<h1>Work</h1>';
+//     const btn = document.createElement('button');
+//     btn.classList.add("add-btn");
+
+//     categoryLabel.appendChild(btn);
+//     workWrapper.appendChild(categoryLabel);
+// }
+
+
+// function for new dropdown
+function dropDown(e) {
+    let target = e.target;
+    if (target.classList.contains("new-btn")) {
+        categoryDropdown.style.display = "flex";
+    } else {
+        categoryDropdown.style.display = "none";
+    }
+}
 
 
 
